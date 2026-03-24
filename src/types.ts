11040d2f884816576c7c35af998db2c85d7362b2
Marius Cetanas <marius.cetanas@icloud.com>
2026-03-24 17:54:01 +0200

    feat: add shared return type interfaces

---
+++ src/types.ts
@@ -0,0 +1,62 @@
+export interface Account {
+  name: string;
+  type: "imap" | "pop" | "iCloud";
+  enabled: boolean;
+  emails: string[];
+}
+
+export interface AccountDetail extends Account {
+  serverName: string;
+  port: number;
+  usesSsl: boolean;
+  userName: string;
+  mailboxCount: number;
+}
+
+export interface Mailbox {
+  name: string;
+  unreadCount: number;
+  accountName: string;
+}
+
+export interface MailboxDetail extends Mailbox {
+  messageCount: number;
+  container: string | null;
+}
+
+export interface MessageSummary {
+  id: number;
+  subject: string;
+  sender: string;
+  dateReceived: string;
+  readStatus: boolean;
+  flagged: boolean;
+  flagIndex: number;
+  hasAttachments: boolean;
+}
+
+export interface MessageDetail extends MessageSummary {
+  toRecipients: Recipient[];
+  ccRecipients: Recipient[];
+  bccRecipients: Recipient[];
+  body: string;
+  headers: string;
+  attachments: Attachment[];
+}
+
+export interface Recipient {
+  name: string;
+  address: string;
+}
+
+export interface Attachment {
+  name: string;
+  mimeType: string;
+  fileSize: number;
+  downloaded: boolean;
+}
+
+export interface AppleScriptError {
+  error: string;
+  errorNumber: number;
+}
